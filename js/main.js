ff5b694350b4fd786c90fc43568ec2d54faa8141
Z1r0chk4 <olha.korviakova@gmail.com>
2020-03-29 14:12:18 +0300

    fix mobile menu

--- js/main.js
+++ js/main.js
@@ -9,18 +9,21 @@ $(document).ready(function() {
     if ($(event.target).closest(".hamburger").length) return;
     $('.hamburger > svg').removeClass('active');
     $('.body').removeClass('open');
+    $('.main_menu').removeClass('open');
     event.stopPropagation();
   });
   
   $(".hamburger").click(function() {
     $('.hamburger > svg').toggleClass('active');
     $(".body").toggleClass("open");
+    $('.main_menu').toggleClass('open');
   });
 
   $('.main_menu a').on('click', function(e) {
     e.preventDefault();
     $('.hamburger > svg').toggleClass('active');
     $('.body').toggleClass('open');
+    $('.main_menu').toggleClass('open');
   });
 
   var $page = $('html, body');
